012b2fdfa77408e4f876e95f44dedf061aff7da9
KrisQ625 <109870555+KrisQ625@users.noreply.github.com>
2022-12-08 19:47:38 +0100

    source code link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (<div className='App'>Weather app</div>
+  return (<div className='App'>Weather app
+  <footer> This project was created by Kristina Vujnovac and is 
+    <a href="https://github.com/KrisQ625/weather-app-react" target="_blank">open sourced</a>
+  </footer>
+  
+  </div>
  
   );
 }
